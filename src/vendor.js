0f87315fafab9ee1b6e7d226b2ef2d2ca59a8f90
runuowbd123 <747263288@qq.com>
2018-01-09 17:27:26 +0800

    添加element-ui-plugin依赖，使用element-ui-plugin提供的elem样式

--- src/vendor.js
+++ src/vendor.js
@@ -5,9 +5,12 @@ import Vue from "vue"
 import Vuex from "vuex"
 import VueRouter from "vue-router"
 import elementUI from "element-ui"
-import "element-ui/lib/theme-chalk/index.css"
+import elementUIPlugin from "element-ui-plugin"
+import "element-ui-plugin/lib/elem-theme/index.css"
+import "element-ui-plugin/lib/theme/index.css"
 import "./ext/font-awesome-4.7.0/css/font-awesome.css"
 
 Vue.use(Vuex)
 Vue.use(VueRouter)
 Vue.use(elementUI)
+Vue.use(elementUIPlugin)
